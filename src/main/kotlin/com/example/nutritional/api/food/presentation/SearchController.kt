60dc3d36b42f5430d55063c8fa5f8110ea585143
Yu Jin <qldls0307@naver.com>
2026-04-25 23:13:23 +0900

    chore: 스웨거 문서 description 깨지는 문제 해결

--- src/main/kotlin/com/example/nutritional/api/food/presentation/SearchController.kt
+++ src/main/kotlin/com/example/nutritional/api/food/presentation/SearchController.kt
@@ -22,7 +22,13 @@ class SearchController(private val foodService: FoodService) {
     @GetMapping
     @Operation(
         summary = "식품 목록 검색",
-        description = "식품명, 조사년도, 제조사, 식품코드로 검색 및 페이징. \\n\\n**[주의] 스웨거 자동완성 값 처리사항**\\n- `sort` 파라미터에 기본으로 들어간 `string` 값을 지우고 `id,desc` 또는 `foodName,asc` 등으로 입력해야 정상 작동합니다. (필요없을 시 항목 자체를 통과시키거나 삭제하세요)\\n- **사용 가능한 정렬 항목 예시**: id, foodName, makerName, researchYear 등"
+        description = """
+            식품명, 조사년도, 제조사, 식품코드로 검색 및 페이징.
+            
+            **[주의] 스웨거 자동완성 값 처리사항**
+            - `sort` 파라미터에 기본으로 들어간 `string` 값을 지우고 `id,desc` 또는 `foodName,asc` 등으로 입력해야 정상 작동합니다. (필요없을 시 항목 자체를 통과시키거나 삭제하세요)
+            - **사용 가능한 정렬 항목 예시**: id, foodName, makerName, researchYear 등
+            """
     )
     fun getFoods(
         @RequestParam(value = "food_name", required = false) foodName: String?,
